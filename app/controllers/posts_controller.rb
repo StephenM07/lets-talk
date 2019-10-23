13c0419759a7488c1b8f8679e7e15e277e2e3979
Stephen Macias <stephenmacias@yahoo.com>
2019-10-23 10:32:15 -0500

    ip address

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    @ip_addr = request.ip
   end
 
   def new 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  resources :posts
-
+  resources :posts do
+    resources :comments, only: :create
+  end
 end
